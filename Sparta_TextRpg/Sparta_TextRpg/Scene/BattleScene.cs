ac423b5ee84ad004686cad6c7372e6d33fda7113
rlaehgus0226 <rlaehgus0226.dev@gmail.com>
2024-05-03 15:48:17 +0900

    회복 물약 사용 ver.2
    딸깍

--- Sparta_TextRpg/Sparta_TextRpg/Scene/BattleScene.cs
+++ Sparta_TextRpg/Sparta_TextRpg/Scene/BattleScene.cs
@@ -86,7 +86,8 @@ namespace Sparta_TextRpg
                 Console.WriteLine($"{i + 1}. 공격");
             }
             Console.WriteLine("A. 스킬사용하기");
-            Console.WriteLine("B. 물약사용하기");
+            Console.WriteLine("S. 물약사용하기");
+
             var key = Console.ReadKey(true).Key;
             if (key >= ConsoleKey.D1 && key < ConsoleKey.D1 + enemies.Count)
             {
@@ -117,36 +118,11 @@ namespace Sparta_TextRpg
                 Console.Clear();
                 Skill();
             }
-            else if (key == ConsoleKey.B)
+            else if (key == ConsoleKey.S)
             {
                 Console.Clear();
-                Console.WriteLine("[보유중인 물약]");
-                List<Item> potions = new List<Item>();
-                foreach (Item item in player._inventory)
-                {
-                    if (item._itemtype == ItemType.POTION)
-                    {
-                        potions.Add(item);
-                    }
-                }
-                if (potions.Count == 0)
-                {
-                    Console.WriteLine("보유중인 물약이 없습니다.");
-                }
-                else
-                {
-                    for (int i = 0; i < potions.Count; i++)
-                    {
-                        Console.WriteLine($"{i + 1}. {potions[i]._name} 회복량: {potions[i]._statvalue}");     
-                        
-                    }                   
-
-                }
-
+                ViewPotion();
             }
-
-
-
             else
             {
                 Console.Clear();
@@ -284,7 +260,7 @@ namespace Sparta_TextRpg
             Console.WriteLine($"레벨업! 현재 레벨: {player._level}, 공격력: {player._attack}, 방어력: {player._defence}");
         }
 
-        /*
+
         private void Reward()
         {
             Random random = new Random();
@@ -319,7 +295,7 @@ namespace Sparta_TextRpg
                     rand = random.Next(1, 101);
                     if (rand <= 70)
                     {
-                        rating = ItemRating.COMMON;
+                        rating = ItemRating.RARE;
                     }
                     else if (rand >= 71 && rand <= 90)
                     {
@@ -337,7 +313,7 @@ namespace Sparta_TextRpg
                         }
                         else
                         {
-                            rating = ItemRating.COMMON;
+                            rating = ItemRating.RARE;
                         }
                     }
 
@@ -357,7 +333,7 @@ namespace Sparta_TextRpg
             }
         }
 
- */
+ 
         private void PlayerAttack(int idx)
         {
             bool critic = false;
@@ -514,7 +490,19 @@ namespace Sparta_TextRpg
             }
         }
 
-        private void ItemHeal()
+        private void ViewPotion()
+        {
+            List<Item> inventory = player._inventory.Where(item => item._itemtype == ItemType.POTION).ToList();
+
+            Console.WriteLine("[보유중인 물약]");
+            for (int i = 0; i < inventory.Count; i++)
+            {
+                Console.WriteLine($"{i + 1}. {inventory[i]._name} 회복량 : {inventory[i]._statvalue} 보유갯수 : {inventory[i]._statvalue}");
+            }
+
+        }
+
+        private void PotionHeal()
         {
             List<Item> potions = new List<Item>();
             foreach (Item item in player._inventory)
